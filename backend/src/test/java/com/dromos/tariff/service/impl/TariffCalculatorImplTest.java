47f2c63bed7e92121333d27d4fc097a75245e2ec
Yam Sol Bertamini <yambertamini.sol@gmail.com>
2025-12-01 03:43:08 -0300

    test(backend): add base for TariffCalculatorImpl

---
+++ backend/src/test/java/com/dromos/tariff/service/impl/TariffCalculatorImplTest.java
@@ -0,0 +1,36 @@
+package com.dromos.tariff.service.impl;
+
+import com.dromos.tariff.model.*;
+import com.dromos.tariff.service.CountryService;
+import com.dromos.tariff.service.TariffCalculator;
+import com.dromos.tariff.service.WtoApiClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class TariffCalculatorImplTest {
+
+    @Mock
+    private WtoApiClient wtoApiClient;
+
+    @Mock
+    private CountryService countryService;
+
+    private TariffCalculator tariffCalculator;
+
+    @BeforeEach
+    void setUp() {
+        tariffCalculator = new TariffCalculatorImpl(wtoApiClient, countryService);
+    }
+
+    
+}
